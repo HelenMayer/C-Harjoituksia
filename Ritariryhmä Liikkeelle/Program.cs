281dc02d8e7f81f01277b1d26f2c613a2ff385d2
HelenMayer <maier.elena0107@gmail.com>
2023-04-01 16:23:27 +0300

    update

--- Ritariryhmä Liikkeelle/Program.cs
+++ Ritariryhmä Liikkeelle/Program.cs
@@ -13,14 +13,54 @@
             int sarakeNumber=Convert.ToInt32(Console.ReadLine());
 
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.Beep();
-            System.Console.WriteLine("Ritari yksi kohteeseen "+(riiviNumber-1)+";"+(sarakeNumber));
-            Console.Beep();
-            System.Console.WriteLine("Ritari kaksi kohteeseen "+riiviNumber+";"+(sarakeNumber+1));
-            Console.Beep();
-            System.Console.WriteLine("Ritari kolme kohteeseen "+riiviNumber+";"+(sarakeNumber-1));
-            Console.Beep();
-            System.Console.WriteLine("Ritari neljä kohteeseen "+(riiviNumber+1)+";"+(sarakeNumber));
+
+            if (riiviNumber-1>0 && sarakeNumber>0 && riiviNumber-1<=8 && sarakeNumber<=8)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Beep();
+                System.Console.WriteLine("Ritari yksi kohteeseen "+(riiviNumber-1)+";"+(sarakeNumber)); 
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                System.Console.WriteLine("Ritari yksi kohteeseen on pois shakkilaudalta"); 
+            }
+                        
+            if (riiviNumber>0 && sarakeNumber+1>0 && riiviNumber<=8 && sarakeNumber+1<=8)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Beep();
+                System.Console.WriteLine("Ritari kaksi kohteeseen "+riiviNumber+";"+(sarakeNumber+1)); 
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                System.Console.WriteLine("Ritari kaksi kohteeseen on pois shakkilaudalta"); 
+            }            
+            
+            if (riiviNumber>0 && sarakeNumber-1>0 && riiviNumber<=8 && sarakeNumber-1<=8)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Beep();
+                System.Console.WriteLine("Ritari kolme kohteeseen "+riiviNumber+";"+(sarakeNumber-1));
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                System.Console.WriteLine("Ritari kolme kohteeseen on pois shakkilaudalta"); 
+            }            
+            
+            if (riiviNumber+1>0 && sarakeNumber>0 && riiviNumber+1<=8 && sarakeNumber<=8)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Beep();
+                System.Console.WriteLine("Ritari neljä kohteeseen "+(riiviNumber+1)+";"+(sarakeNumber));
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                System.Console.WriteLine("Ritari neljä kohteeseen on pois shakkilaudalta"); 
+            }
 
         }
     }
